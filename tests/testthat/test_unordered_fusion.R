3abef4dcdcd2ecf69c63eb9d5d8c3f80fc5e08f9
Luofeng Liao <luofengl@student.unimelb.edu.au>
2018-07-17 11:12:17 +0800

    Add test file
    Tests for unordered fusion lasso

---
+++ tests/testthat/test_unordered_fusion.R
@@ -0,0 +1,13 @@
+
+
+# Special case 1:
+# Large lambda: lambdas in each group are
+# the means of everything in the group
+
+# Special case 2:
+# For every lambda: when only w_ij = 1 (j = i+1)
+# it becomes ordered fused lasso
+
+# Special case 3:
+# For evry lambda: when all i,j w_ij = 1
+# the solution path contains no split.
